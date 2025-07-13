5f96f092c8581d0e76242382faddbb931f2b1257
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-07-13 21:19:29 +0600

    work related card section

--- src/components/portfolio/Brand.tsx
+++ src/components/portfolio/Brand.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import CommonWrapper from "../shared/CommonWrapper";
 import CommonTitle from "../reuse/CommonTitle";
 import Background from "@/assets/images/mooneffect.png";
 const Brand = () => {
@@ -8,7 +7,7 @@ const Brand = () => {
       {/* brand section */}
       <section className="relative -top-80 2xl:-top-28 w-full">
         <div
-          className="relative inset-0 w-full rotate-180 bg-no-repeat bg-center bg-contain min-h-[360px] 
+          className="relative inset-0 w-full rotate-180 bg-no-repeat bg-center bg-contain min-[360px] 
         sm:min-h-[600px] 
         md:min-h-[700px] 
         lg:min-h-[800px] 
@@ -20,7 +19,10 @@ const Brand = () => {
           }}
         >
           <div className="rotate-180 absolute inset-0">
-            <h1 className="text-white">we must provide with your brand</h1>
+            <CommonTitle
+              text="we must provide
+              with your brand"
+            />
           </div>
         </div>
       </section>
--- src/components/reuse/CommonTitle.tsx
+++ src/components/reuse/CommonTitle.tsx
@@ -1,6 +1,6 @@
 const CommonTitle = ({ text }: { text: string }) => {
   return (
-    <h1 className="bg-[linear-gradient(225deg,_#E6FAF7_0%,_#00D1AE_90.38%)] bg-clip-text text-transparent leading-[140%] text-[1.5rem] sm:text-[2.2rem] lg:text-[3.2rem] font-semibold max-w-1/2 mx-auto text-center">
+    <h1 className="bg-[linear-gradient(225deg,_#E6FAF7_0%,_#00D1AE_90.38%)] bg-clip-text text-transparent leading-[140%]  mx-auto text-[clamp(2rem,10vw,3rem)] font-semibold  text-center">
       {text}
     </h1>
   );
